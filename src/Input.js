82d2a128e8b8444399297517c7995bf197fbb737
Jordan <jordanleelong5@gmail.com>
2020-11-21 15:40:39 -0800

    wrote test to check and make sure we are passing in props and it is the correct data type

--- src/Input.js
+++ src/Input.js
@@ -1,12 +1,18 @@
 import React from 'react'; 
+import PropTypes from 'prop-types';
 
-const Input = (props) => {
+
+const Input = ({ secretWord }) => {
     return (
         <div data-test='input-component'>
 
         </div>
     );
 };
+// PropTypes are a mechanism to ensure that components use the correct data type and pass the right data as props.
+Input.propTypes = {
+    secretWord: PropTypes.string.isRequired
+}
 
 
 export default Input; 
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,12 +1,21 @@
+// import checkPropTypes from 'check-prop-types';
 import {shallow} from 'enzyme'; 
 import Input from './Input'; 
-import {findByTestAttr} from './test/testUtils'
+import {findByTestAttr, checkProps} from './test/testUtils'
 
 
-const setUp = () => shallow(<Input />); 
+const setUp = (secretWord='party') => { // add prop here to pass checkProps test.
+   return shallow(<Input secretWord={secretWord}/>); // added prop here
+    
+}
 
 test('renders the componnent without throwing an error', () => {
     const wrapper = setUp(); 
     const component = findByTestAttr(wrapper, 'input-component'); 
     expect(component.length).toBe(1); 
+}); 
+
+test('does not throw warning with expected props', () => {
+    checkProps( {secretWord: 'party'}); 
+
 }); 
