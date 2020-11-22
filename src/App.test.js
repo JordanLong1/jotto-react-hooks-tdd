d46c67ee9e244eca588a7fb7910409c5918342f6
Jordan <jordanleelong5@gmail.com>
2020-11-21 22:11:22 -0800

    added render without error, getSecretWord called on mount, and secretWord doesnt update on app update

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
+import React from 'react'
 import App from './App';
-import {shallow} from 'enzyme'; 
+import {mount} from 'enzyme'; 
 import {findByTestAttr} from './test/testUtils'
+import hookActions from './actions/hookActions'
+
+
+const mockGetSecretWord = jest.fn(); 
+ 
 
 const setUp = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear(); 
+  hookActions.getSecretWord = mockGetSecretWord;
+  return mount(<App />);
 }
 
 
@@ -11,4 +19,24 @@ test('renders without error', () => {
   const wrapper = setUp(); 
   const component = findByTestAttr(wrapper, 'app-component'); 
   expect(component.length).toBe(1);
+});
+
+describe('getSecretWord calls', () => {
+
+  test('getSecretWord gets called on app mount', () => {
+    setUp(); 
+
+    //check to see if secret word was updated 
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
+
+  test('secretWord does not update on App update', () => {
+    const wrapper = setUp(); 
+    mockGetSecretWord.mockClear();
+
+      wrapper.setProps(); 
+      expect(mockGetSecretWord).not.toHaveBeenCalled();
+
+
+  });
 });
