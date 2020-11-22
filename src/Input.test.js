55ef3458963d8c8d3b5d9b7a571be592bdc02988
Jordan <jordanleelong5@gmail.com>
2020-11-21 16:55:47 -0800

    added submit button test and test to make state set back to an empty string after submit

--- src/Input.test.js
+++ src/Input.test.js
@@ -16,22 +16,34 @@ test('renders the componnent without throwing an error', () => {
     expect(component.length).toBe(1); 
 }); 
 
+test('finds submit button without throwing an error', () => {
+    const wrapper = setUp(); 
+    const submitButton = findByTestAttr(wrapper, 'submit-button'); 
+    expect(submitButton.length).toBe(1);
+});
+
 test('does not throw warning with expected props', () => {
     checkProps( {secretWord: 'party'}); 
-
 });
 
 describe('state controller input field', () => {
-    test('state updates with value onChage', () => {
-         const mockSetCurrentGuess = jest.fn(); 
-         React.useState = jest.fn(() => ['', mockSetCurrentGuess]); 
-
-         const wrapper = setUp(); 
+    let mockSetCurrentGuess = jest.fn(); 
+    let wrapper; 
+    beforeEach(() => {
+        mockSetCurrentGuess.mockClear()
+        React.useState = jest.fn(() => ['', mockSetCurrentGuess]); 
+        wrapper = setUp();
+    })
+    test('state updates with value onChange', () => {
          const inputBox = findByTestAttr(wrapper, 'input-box'); 
-
          const mockEvent = {target: {value: 'train'} }; 
          inputBox.simulate('change', mockEvent); 
-
          expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
     }); 
+
+    test('setCurrentGuess resets back to an empty string onSubmit', () => {
+        const submitButton = findByTestAttr(wrapper, 'submit-button'); 
+        submitButton.simulate('click', { preventDefault: () => {}}); 
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith('')
+    });
 }); 
