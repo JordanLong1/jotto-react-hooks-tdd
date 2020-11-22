753e5901470ce10011d6aac48a4fe184905868c3
Jordan <jordanleelong5@gmail.com>
2020-11-21 22:10:14 -0800

    added eslint disable and exported default for useEffect

--- src/actions/hookActions.js
+++ src/actions/hookActions.js
@@ -1,8 +1,10 @@
-import React from 'react'; 
+/* eslint-disable import/no-anonymous-default-export */
 import axios from 'axios'
 export const getSecretWord = async (setSecretWord) => {
-    const response = await axios.get('http://localhost:3030'); 
-    setSecretWord(response.data); 
+    const response = await axios.get('http://localhost:3030');
+    setSecretWord(response.data);
 }
 
-export default getSecretWord
+export default {
+    getSecretWord,
+  }
