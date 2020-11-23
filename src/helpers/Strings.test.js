c9769099c45f467689c3a148af927730ab0a7c4e
Jordan <jordanleelong5@gmail.com>
2020-11-22 23:36:13 -0800

    wrote out tests for language, emoji, mermish

--- src/helpers/Strings.test.js
+++ src/helpers/Strings.test.js
@@ -1,18 +1,29 @@
 import stringsModule from './Strings';
 const {getStringByLanguage} = stringsModule;
 
-test('returns correct submit string for english', () => {
 
+const strings = {
+    en: {english: 'submit'}, 
+    emoji: {submit: '🚀'}, 
+    mermish: {}
+}
+
+test('returns correct submit string for english', () => {
+    const string = getStringByLanguage('en', 'submit', strings); 
+    expect(string).toBe('submit');
 });
 
 test('returns the correct submit string for emoji', () => {
-
+    const string = getStringByLanguage('emoji', 'submit', strings); 
+    expect(string).toBe('🚀');
 }); 
 
 test('returns english submit string when language doesnt exist', () => {
-
+    const string = getStringByLanguage('notValidLanguage', 'submit', strings); 
+    expect(string).toBe('submit');
 }); 
 
 test('returns english stiring submit when submit key does not exist for language', () => {
-
+    const string = getStringByLanguage('mermish', 'submit', strings); 
+    expect(string).toBe('submit');
 });
