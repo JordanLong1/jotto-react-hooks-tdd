480fcf6c308ab9bb7038bd580e89acf62da2a474
Jordan <jordanleelong5@gmail.com>
2020-11-22 23:31:10 -0800

    wrote skeleton for tests needed

---
+++ src/helpers/Strings.test.js
@@ -0,0 +1,18 @@
+import stringsModule from './Strings';
+const {getStringByLanguage} = stringsModule;
+
+test('returns correct submit string for english', () => {
+
+});
+
+test('returns the correct submit string for emoji', () => {
+
+}); 
+
+test('returns english submit string when language doesnt exist', () => {
+
+}); 
+
+test('returns english stiring submit when submit key does not exist for language', () => {
+
+});
