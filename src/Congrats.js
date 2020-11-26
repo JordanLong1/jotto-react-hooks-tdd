7914b810a2dd2b80aa31fba7849f6c7e0019f5c6
Jordan <jordanleelong5@gmail.com>
2020-11-26 12:04:35 -0800

    removed jsdoc and comments

--- src/Congrats.js
+++ src/Congrats.js
@@ -2,12 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import languageContext from './contexts/LanguageContext'
 import stringsModule from './helpers/Strings'
-/**
- * Functional react component for congratulatory message.
- * @function
- * @param {object} props - React props.
- * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
- */
+
 const Congrats = (props) => {
 
 
